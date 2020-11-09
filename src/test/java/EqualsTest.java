cecdb97ee4764198b8fcb7dd00cdcb82945ace06
王冰 <wang.bing@datatom.com>
2020-11-09 22:37:12 +0800

    syn

--- src/test/java/EqualsTest.java
+++ src/test/java/EqualsTest.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
 
+import java.util.*;
+
 /**
  * @author bing.wang
  * @date 2020/8/30 00:07
@@ -11,5 +13,45 @@ public class EqualsTest {
         Integer b = 1;
 
         System.out.println(a.equals(b));
+    }
+
+    @Test
+    public void convertStringMap(){
+        HashMap<String, String> colmap = new HashMap<>();
+        String s = "a:a1,b:b1,c:c1";
+        List<String> strings = Arrays.asList(s.split(","));
+        for (String cols:strings){
+            String[] col = cols.split(":");
+            colmap.put(col[0],col[1]);
+        }
+
+
+        Set<String> set = colmap.keySet();
+        for(String str:colmap.keySet()){
+            String s1 = colmap.get("a") == null ? colmap.get(str) : "hello";
+            System.out.println(s1);
+        }
+
+        //System.out.println(colmap.get("1") == null);
+
+
+    }
+
+    @Test
+    public void test(){
+        String s = "abc d";
+
+        boolean equals = s.equals("");
+        System.out.println(equals);
+
+        boolean empty = s.isEmpty();
+        System.out.println(empty);
+
+        System.out.println(s.length());
+
+        char[] chars = s.toCharArray();
+        for(char c :chars){
+            System.out.println(c);
+        }
     }
 }
---
+++ src/test/java/test.java
@@ -0,0 +1,6 @@
+/**
+ * @author bing.wang
+ * @date 2020/9/24 11:35
+ */
+public class test {
+}
